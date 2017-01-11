76b21a82224bbe7b6fe2335c2eeb3f5bbde81506
Peter <peterimoesi@yahoo.com>
2017-01-11 13:18:11 +0200

    initial commit

--- myapp/public/javascripts/calender.js
+++ myapp/public/javascripts/calender.js
@@ -4,7 +4,7 @@ function date() {
 	var date = new Date(); 
 	var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 	dt = " " + days[date.getDay()] + " " + date.getMonth() + ", " + date.getFullYear();
-	$('input[type=text]').attr('placeholder', dt)
+	$('#usr').attr('placeholder', dt)
 	console.log(dt)
 }
 date();
