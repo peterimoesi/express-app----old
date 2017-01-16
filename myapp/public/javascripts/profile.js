382a162822b8524ce6ec2459a2ed38cfccffec71
peterimoesi <peterimoesi@yahoo.com>
2017-01-16 22:19:48 +0200

    Update profile.js

--- myapp/public/javascripts/profile.js
+++ myapp/public/javascripts/profile.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
         e.preventDefault();
         $(this).effect( "bounce", { times : 3 }, "slow");
 	    var dialog = document.getElementById('my-obj');  
-        	setTimeOut(function(){dialog.show()}, 2000);
+        	setTimeout(function(){dialog.show()}, 2000);
        	 console.log("showing")    
     });
 	document.getElementById('clickOff').onclick = function() {  
