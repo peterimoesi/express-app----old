92e325b200a1e344d12126fb03418b5c4cb96bf7
peterimoesi <peterimoesi@yahoo.com>
2017-01-16 21:55:27 +0200

    Update profile.js

--- myapp/public/javascripts/profile.js
+++ myapp/public/javascripts/profile.js
@@ -2,10 +2,17 @@ $(document).ready(function(){
 	$('#about-me-img').on('click', function (e) {
         e.preventDefault();
         $(this).effect( "bounce", { times : 3 }, "slow");
+	    var dialog = document.getElementById('my-obj');  
+        	dialog.show();
+       	 console.log("showing")    
     });
+	document.getElementById('clickOff').onclick = function() {  
+        dialog.close();
+        console.log("closed"); 
+    };
 
 	$('#codewars').click(function(){
 		$(this).load("https://www.codewars.com/users/peterimoesi");
-	})
+	});
     
 });
