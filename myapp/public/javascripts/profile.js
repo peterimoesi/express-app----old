5f5776ca7873670ca52c3a66be44f40007776edf
Peter BIG-P Imoesi <Peter BIG-P Imoesi>
2017-01-18 15:43:18 +0200

    mob and 1366

--- myapp/public/javascripts/profile.js
+++ myapp/public/javascripts/profile.js
@@ -5,18 +5,15 @@ $(document).ready(function(){
 		$('#about-me-img').on('click', function (e) {
         e.preventDefault();
         $(this).effect( "bounce", { times : 3 }, "slow");
-	    var dialog = document.getElementById('my-obj');  
-        	setTimeout(function(){dialog.show()}, 1000);
+  
+        	setTimeout(function(){$('#my-obj').show()}, 1000);
        	 console.log("showing")    
     });
 	document.getElementById('clickOff').onclick = function() {  
-        document.getElementById('my-obj').close();
+        $('#my-obj').hide();
         console.log("closed"); 
     };
-	$('body').on('click', function(){
-	  	document.getElementById('my-obj').close();
-	  });
-
+	
 	$('#codewars').click(function(){
 		$(this).load("https://www.codewars.com/users/peterimoesi");
 	});
